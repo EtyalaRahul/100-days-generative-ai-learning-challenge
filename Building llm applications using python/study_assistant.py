0c578918d6474e8ed83ed3cb0e0fbe7afba9db48
EtyalaRahul <2300032656@kluniversity.in>
2026-07-06 21:54:35 +0530

    day1 study assistant

--- Building llm applications using python/study_assistant.py
+++ Building llm applications using python/study_assistant.py
@@ -1,1 +1,17 @@
+from google import genai
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+def study_assistant(user_prompt):
+  response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=user_prompt
+  )
+  return response
+
+output = study_assistant("Explain Generative AI")
+print(output.text)
